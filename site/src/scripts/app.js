768397f8483e0f6293c8d2e5045665ae08eb91fa
Emmanuelle Misson <em.misson@gmail.com>
2016-05-22 00:06:29 +0200

    added step 3

--- site/src/scripts/app.js
+++ site/src/scripts/app.js
@@ -1,3 +1,24 @@
 $(function() {
 	console.log('test');
+  
+  $('.js-pasteButton').click(function(e){
+    console.log("MAIS PASTE MOI !");
+    return false;
+  });
+  
+  $('.js-ListDisplay').click(function(e){
+    if ($(this).hasClass('js-ListDisplay--list')){
+      $('.templatesList').addClass('templatesList--list');
+      $('.js-ListDisplay').addClass('opacity');
+      $(this).removeClass('opacity');
+    }
+    else{
+      $('.templatesList').removeClass('templatesList--list');
+      $('.js-ListDisplay').addClass('opacity');
+      $(this).removeClass('opacity');
+    }
+  });
+  
 });
+
+
