98a0f6a0af019f914ecb3f9a65b99b9b46f0aa76
Thomas Iturralde <lainkk@gmail.com>
2016-05-30 09:50:29 +0200

    Squashed commit of the following:
    commit 8b65aeaed41ee911f051c2d319bbc627caf6763f
    Author: Emmanuelle Misson <em.misson@gmail.com>
    Date:   Fri May 27 11:46:15 2016 +0200
    
        update
    
    commit afcf5e8d44f1e824e5f6702ccb58c876bcdd43d3
    Author: Emmanuelle Misson <em.misson@gmail.com>
    Date:   Fri May 27 09:48:21 2016 +0200
    
        hiding paste button when #projectname is empty
    
    commit c61253cd27d3fd954f33d334a747df6e593c9eb2
    Author: Emmanuelle Misson <em.misson@gmail.com>
    Date:   Thu May 26 11:42:35 2016 +0200
    
        Fake paste button
    
    commit 82581158127602b91fa3e99cb6f8ef968d562a5c
    Author: Emmanuelle Misson <em.misson@gmail.com>
    Date:   Thu May 26 11:38:48 2016 +0200
    
        hotfixes
    
        grid/list with improve
        section and general display improve
    
    commit b3ca5261eb218daca3ee9a135f57df3305ba9ad3
    Author: Emmanuelle Misson <em.misson@gmail.com>
    Date:   Thu May 26 10:47:22 2016 +0200
    
        Pixel perfect pour faire plaisir a Lucas
    
        ALO UI CER AL POLICE
    
    commit a07dc63a47c8af9fd6784f86f42640d5174ea08c
    Author: Emmanuelle Misson <em.misson@gmail.com>
    Date:   Wed May 25 12:15:08 2016 +0200
    
        WIP inputs
    
        edit project name button
        blue label on input focus
    
    commit b96fde62225cbb3fa8862141148e1a4cabe2c52a
    Author: LucasGuarneri <gclucaslp@gmail.com>
    Date:   Wed May 25 11:24:22 2016 +0200
    
        removed a picture of Sivolc
    
    commit a2b5f92b8514a2181a4c117eeee5ba09d6f02429
    Author: LucasGuarneri <gclucaslp@gmail.com>
    Date:   Wed May 25 11:16:37 2016 +0200
    
        added a picture of Sivolc
    
    commit 8fe47091105fd8dd36ef4d12296795680427310c
    Author: Emmanuelle Misson <em.misson@gmail.com>
    Date:   Tue May 24 12:02:55 2016 +0200
    
        edit project title
    
    commit 55908787e804e7420a01c79df7600fb56bafbc25
    Author: Emmanuelle Misson <em.misson@gmail.com>
    Date:   Tue May 24 11:03:49 2016 +0200
    
        step 3 - switch between list and grid display
    
    commit 768397f8483e0f6293c8d2e5045665ae08eb91fa
    Author: Emmanuelle Misson <em.misson@gmail.com>
    Date:   Sun May 22 00:06:29 2016 +0200
    
        added step 3
    
    commit 90c5751f38f7ddde1904d834468470b94076d412
    Author: Emmanuelle Misson <em.misson@gmail.com>
    Date:   Sun May 22 00:06:18 2016 +0200
    
        added step2
    
    commit 4b9601d53ee648feb20dc3236b5f24ccde506d66
    Author: Emmanuelle Misson <em.misson@gmail.com>
    Date:   Sun May 22 00:05:48 2016 +0200
    
        fix for js watch
    
    commit dfe1550a17a893396585167e3fba1b5697222f69
    Author: Emmanuelle Misson <em.misson@gmail.com>
    Date:   Thu May 12 12:15:12 2016 +0200
    
        added buttons styles / added step 2 page
    
    commit 64539397d49666b9ab331e36fc89aa8ad57654b3
    Author: Emmanuelle Misson <em.misson@gmail.com>
    Date:   Wed May 11 12:03:28 2016 +0200
    
        wit home page

--- site/src/scripts/app.js
+++ site/src/scripts/app.js
@@ -1,3 +1,78 @@
 $(function() {
-	
+	console.log('Hello Tagy Gérec');
+  /* */
+  /* paste buttons */
+  $('.js-pasteButton--project').click(function(){
+    console.log("MAIS PASTE MOI !");
+    $('.js-pasteInput').val("http://asos.com");
+    $('.js-pasteInput').focus();
+  });
+  
+  $('.js-pasteButton--page').click(function(){
+    console.log("MAIS PASTE MOI !");
+    $('.js-pasteInput').val("http://asos/product.com");
+    $('.js-pasteInput').focus();
+  });
+  
+  $('#project-url').focus(function(){
+    $('.js-pasteButton--project').removeClass('u-hidden');
+  });
+  $('#project-url').blur(function(){
+    $('.js-pasteButton--project').addClass('u-hidden');
+  });
+  
+  $('#page-url').focus(function(){
+    $('.js-pasteButton--page').removeClass('u-hidden');
+  });
+  $('#page-url').blur(function(){
+    $('.js-pasteButton--page').addClass('u-hidden');
+  });
+  
+  /* */
+  /* edit project name button */
+  /* */
+  $('.js-editIcon').click(function(){
+    /* empty the field to prevent cursor to be at the beginning of the field */
+    var temp = $('#project-name').val();
+    $('#project-name').val('');
+    $('#project-name').val(temp);
+    
+    $('#project-name').show();
+    $('#project-name').focus();
+  });
+  
+  $('#project-name').focus(function(){
+    $('.js-editIcon').addClass('u-hidden');
+  });
+  
+  $('#project-name').blur(function(){
+    $('.js-editIcon').removeClass('u-hidden');
+  });
+  
+  /* */
+  /* change label color on input focus */
+  $('input').focus(function(){
+    $(this).closest('.formGroup').find('label').addClass('focused');
+  });
+  
+  $('input').blur(function(){
+    $(this).closest('.formGroup').find('label').removeClass('focused');
+  });
+  
+
+  /* step 3 */
+  /* switch between list and grid display for templates list*/
+  $('.js-ListDisplay').click(function(e){
+    if ($(this).hasClass('js-ListDisplay--list')){
+      $('.templatesList').addClass('templatesList--list').removeClass('templatesList--grid');
+      $('.js-ListDisplay').removeClass('displaySwitch--selected');
+      $(this).addClass('displaySwitch--selected');
+    }
+    else if ($(this).hasClass('js-ListDisplay--grid')){
+      $('.templatesList').addClass('templatesList--grid').removeClass('templatesList--list');
+      $('.js-ListDisplay').removeClass('displaySwitch--selected');
+      $(this).addClass('displaySwitch--selected');
+    }
+  });
+  
 });
