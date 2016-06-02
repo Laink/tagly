7cab73e9af7cb44b8d999553ff6a9934c3d577dd
Thomas Iturralde <lainkk@gmail.com>
2016-06-02 15:07:46 +0200

    remove site folder

---
+++ src/scripts/app.js
@@ -0,0 +1,124 @@
+$(function() {
+	console.log('Hello Tagy Gérec');
+  
+  var urls =["asos.com/1.html", "asos.com/2.html", "asos.com/3.html", "asos.com/1.html", "asos.com/2.html", "asos.com/3.html"];
+  var images =["http://fakeimg.pl/144x90/", "http://fakeimg.pl/143x90/", "http://fakeimg.pl/142x90/", "http://fakeimg.pl/144x90/", "http://fakeimg.pl/143x90/", "http://fakeimg.pl/142x90/"];
+  var count = 0;
+  
+  /* */
+  /* paste buttons */
+  $('.js-pasteButton--project').click(function(){
+    $('.js-pasteInput').val("http://asos.com");
+    $('.js-pasteInput').focus();
+  });
+  
+  $('.js-pasteButton--page').click(function(){
+    $('.js-pasteInput').val(urls[count]);
+    count++;
+    $('.js-pasteInput').focus();
+  });
+  
+  $('#project-url').focus(function(){
+    $('.js-pasteButton--project').removeClass('u-hidden');
+  });
+  $('#project-url').blur(function(){
+    $('.js-pasteButton--project').addClass('u-hidden');
+  });
+  
+  $('#page-url').focus(function(){
+    $('.js-pasteButton--page').removeClass('u-hidden');
+  });
+  $('#page-url').blur(function(){
+    $('.js-pasteButton--page').addClass('u-hidden');
+  });
+  
+  /* */
+  /* edit project name button */
+  /* */
+  $('.js-editIcon').click(function(){
+    /* empty the field to prevent cursor to be at the beginning of the field */
+    var temp = $('#project-name').val();
+    $('#project-name').val('');
+    $('#project-name').val(temp);
+    
+    $('#project-name').show();
+    $('#project-name').focus();
+  });
+  
+  $('.project-name--index').trigger("focus");
+    
+  $('#project-name').focus(function(){
+    $('.js-editIcon').addClass('u-hidden');
+  });
+  
+  $('#project-name').blur(function(){
+    $('.js-editIcon').removeClass('u-hidden');
+  });
+  
+  /* */
+  /* change label color on input focus */
+  $('input').focus(function(){
+    $(this).closest('.formGroup').find('label').addClass('focused');
+  });
+  
+  $('input').blur(function(){
+    $(this).closest('.formGroup').find('label').removeClass('focused');
+  });
+  
+
+  /* step 3 */
+  /* switch between list and grid display for templates list*/
+  $('.js-ListDisplay').click(function(e){
+    if ($(this).hasClass('js-ListDisplay--list')){
+      $('.templatesList').addClass('templatesList--list').removeClass('templatesList--grid');
+      $('.js-ListDisplay').removeClass('displaySwitch--selected');
+      $(this).addClass('displaySwitch--selected');
+    }
+    else if ($(this).hasClass('js-ListDisplay--grid')){
+      $('.templatesList').addClass('templatesList--grid').removeClass('templatesList--list');
+      $('.js-ListDisplay').removeClass('displaySwitch--selected');
+      $(this).addClass('displaySwitch--selected');
+    }
+  });
+  
+  /* select */
+  
+  $('.js-select-label').click(function(e){
+    e.stopPropagation();
+    $('.js-select').toggleClass('select--opened');
+  });
+  
+  $('.js-select-close').click(function(e){
+    e.stopPropagation();
+    $('.js-select').removeClass('select--opened');
+  });
+  $('.select-option').click(function(e){
+    var res = $(e.target).text();
+    console.log(res);
+    $('.select-labelTxt').text(res);
+    $('.js-select').addClass('select--chosen');
+    $('.js-select').removeClass('select--opened');
+  });
+  
+  $('.js-add-template').click(function(){
+    var value=  $('.select-labelTxt').text();
+    var item = "<li class=\"templatesListItem\"> <img src=\""+images[count]+"\" class=\"templatesListImage\"><div class=\"templatesListLabel\"><p class=\"templatesListName\">"+value+"</p></div></li>";
+    $('.templatesList').append(item);
+    $('#page-url').val('');
+    $('.select-labelTxt').text('Select a template type');
+    $('.js-select').removeClass('select--chosen');
+    return false;
+  });
+  
+  $('body').click(function(e){
+    $('.js-select').removeClass('select--opened');
+  });
+  
+  $(function() {
+    $('.iframe-loader span').animate({
+        width: '100%'
+      }, 2000, function() {
+        $(this).parent().parent().fadeOut();
+    });
+  }); 
+});
